9094d64e747fb264639201e0104f45f6930c8da7
davidkakov111 <143946579+davidkakov111@users.noreply.github.com>
2025-03-13 21:38:10 +0200

    UI header with logo

--- SyscodeCli/src/app/app.component.ts
+++ SyscodeCli/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -16,4 +16,21 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  currentRoute: string = '';
+
+  constructor(
+    private router: Router,
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  };
+
+  // Check if the current route is active
+  isActive(route: string): boolean {
+    return this.currentRoute === route;
+  }
+}
